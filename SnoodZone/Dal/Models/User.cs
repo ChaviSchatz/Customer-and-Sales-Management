fb9832527769d014fa428f20268dc2aa4835ca1d
BatShevaYamin <hand2b13@mbjcopm.org.il>
2023-02-05 14:01:39 +0200

    add STORE

--- SnoodZone/Dal/Models/User.cs
+++ SnoodZone/Dal/Models/User.cs
@@ -7,6 +7,7 @@ partial class User
     public string Name { get; set; }
     public string PhoneNumber { get; set; }
     public Address Address { get; set; }
+    public string StoreName { get; set; }
     public List<Order> Orders { get; set; }
 }
 
