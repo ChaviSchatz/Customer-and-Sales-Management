0881c8c8b5a857e90530813696bee3139d41b248
ChaviSchatz <hand2b26@mbjcomp.org.il>
2023-02-05 13:26:39 +0200

    hi bat sheva

--- SnoodZone/Dal/Models/User.cs
+++ SnoodZone/Dal/Models/User.cs
@@ -1,11 +1,13 @@
 ﻿
 
-
-
 partial class User
     {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public Address Address { get; set; }
+    public List<Order> Orders { get; set; }
 }
 
