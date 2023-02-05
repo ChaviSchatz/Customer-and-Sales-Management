c6232155249122b6c619a6d0a38729971e634b1c
BatShevaYamin <hand2b13@mbjcopm.org.il>
2023-02-05 13:26:50 +0200

    Create partial

--- SnoodZone/Dal/Models/User.cs
+++ SnoodZone/Dal/Models/User.cs
@@ -1,11 +1,10 @@
 ﻿
-
-
-
 partial class User
-    {
+{
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
+    public string StoreName { get; set; }
+    public string EmailAddress { get; set; }
 }
 
