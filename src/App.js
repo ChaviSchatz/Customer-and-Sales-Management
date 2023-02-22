f9802291429209c75536b525f547da2692c2cb7d
ChaviSchatz <hand2b26@mbjcomp.org.il>
2023-02-22 16:51:33 +0200

    create login form without fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Login } from './final project/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Login></Login>
       </header>
     </div>
   );
---
+++ src/final project/Login.js
@@ -0,0 +1,55 @@
+import React from "react";
+import './login.css';
+import {useForm} from "react-hook-form";
+
+
+
+
+export function Login() {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const handleRegistration = (data) => console.log(data);
+    const registerOptions = {
+        email: { required: "Email is required" },
+        password: {
+            required: "Password is required",
+            minLength: {
+                value: 8,
+                message: "Password must have at least 8 characters"
+            }
+        }
+    };
+
+    return (
+        <>
+            --------------------- React hook form ---------------------
+        <form className="form" onSubmit={handleSubmit(handleRegistration)}>
+            
+                <div class="form-outline mb-4">
+                    <label class="form-label" for="form2Example1">Email</label>
+                    <input id="form2Example1" class="form-control"
+                        type="email"
+                        name="email"
+                        {...register('email', registerOptions.email)}
+                    />
+                    <small className="text-danger">
+                        {errors ?.email && errors.email.message}
+                    </small>
+                </div>
+                
+                <div class="form-outline mb-4">
+                    <label class="form-label" for="form2Example2">Password</label>
+                    <input id="form2Example2" class="form-control"
+                        type="password"
+                        name="password"
+                        {...register('password', registerOptions.password)}
+                    />
+                    <small className="text-danger">
+                        {errors ?.password && errors.password.message}
+                    </small>
+                </div>
+                <button class="btn btn-primary btn-block mb-4">Submit</button>
+            </form>
+        </>
+    );
+}
