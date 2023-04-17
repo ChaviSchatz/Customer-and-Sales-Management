9321ab90679d93203e50b5bb1abe11b0203796a0
ChaviSchatz <hand2b26@mbjcomp.org.il>
2023-04-17 19:44:14 +0300

    יום שני כז

--- src/final project/Home.js
+++ src/final project/Home.js
@@ -9,23 +9,41 @@ export function Home(state) {
     const postUrl = urlUsers + `/${index}`;
     const users = useSelector((state) => state.usersReducer);
     const user = users[index];
-    const [details, setDetails] = useState(null);
+    const details = useRef({});
+    const [b, setB] = useState();
+   
 
     useEffect(() => {
-        const innerFunc = () => {
-            const res = axios.post(postUrl, user)
-                .then((response) => {setDetails(response.data);
-                    console.log(response.data)
-                })
-                .catch((error) => console.log(error));
-            console.log(details);
-        }
-        innerFunc();
+        debugger
+        const r = axios.get(urlUsers).then((response) => {
+            // setB(response.data);
+            console.log(response.data);
+            console.log(b);
+
+        })
+        .catch((error) => console.log(error));
+        const res = axios.post(postUrl, user)
+        .then((response) => {
+            console.log(response.status);
+            if (response.status < 300) {
+                details.current = response.data;
+                
+                console.log(response.data.emailAddress);
+                console.log(details.current.emailAddress);
+            }
+            else {
+                console.log("the http request faild");
+                ///add ups there is a problem////
+            }
+        })
+        .catch((error) => console.log(error));
+    console.log(details);
     }, []);
 
     return (
-        <> 
-            <p>hello {details}</p>
+        <>
+            {/* <p>hello {b[0].emailAddress}</p> */}
+            {/* <p>hello {details.current.emailAddress}</p> */}
         </>
     );
 }
