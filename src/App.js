4fdc241a8fe44ac5f119da2347d0c1c833fee89c
BatShevaYamin <hand2b13@mbjcopm.org.il>
2023-04-17 19:46:36 +0300

    ייייי

--- src/App.js
+++ src/App.js
@@ -6,20 +6,24 @@ import { Home } from './final project/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import {store} from './final project/redax/store';
+import {Update} from './final project/Update';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Provider store={store}>
-        <BrowserRouter>
+        {/* <BrowserRouter>
           <Routes>
             <Route exact path="/" element={<Login />} />
             <Route exact path="Signup" element={<Singup />} />
             <Route exact path="/home/:index" element={<Home />} />
+            <Route exact path="/updateDetails/:index" element={<Update />} />
           </Routes>
-        </BrowserRouter>
-        </Provider>
+        </BrowserRouter> */}
+        <Update></Update>
+        </Provider> 
+        
        
       </header>
     </div>
