1f3b8b9cbcf5d7907128397d85e9843083ad5d55
ChaviSchatz <hand2b26@mbjcomp.org.il>
2023-04-17 13:16:17 +0300

    עכשיו ממש

--- src/final project/Home.js
+++ src/final project/Home.js
@@ -1,29 +1,31 @@
 import axios from "axios";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { urlUsers } from "./endpoints.ts";
 
 export function Home(state) {
-    const postUrl = urlUsers + "/userDetails";
     const { index } = useParams();
+    const postUrl = urlUsers + `/${index}`;
     const users = useSelector((state) => state.usersReducer);
     const user = users[index];
-    const details = useRef("hhhh");
+    const [details, setDetails] = useState(null);
 
     useEffect(() => {
-        const innerFunc = async () => {
-            const res = await axios.post(postUrl, user)
-                .then((response) => details.current = response.data)
+        const innerFunc = () => {
+            const res = axios.post(postUrl, user)
+                .then((response) => {setDetails(response.data);
+                    console.log(response.data)
+                })
                 .catch((error) => console.log(error));
-            console.log(details.current);
+            console.log(details);
         }
         innerFunc();
     }, []);
 
     return (
         <> 
-            <p>hello {details.current}</p>
+            <p>hello {details}</p>
         </>
     );
 }
